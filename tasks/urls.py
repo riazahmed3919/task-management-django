e12f662ff3761f61bfa336091881795fb699775b
Riaz Ahmed <riazahmed3919@gmail.com>
2025-06-16 01:09:57 +0600

    Dynamic ulrs implemented

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import show_task
+from .views import show_task, show_specific_task
 
 urlpatterns = [
     path('show_task/', show_task)
+    path('show_task/<int:id>/', show_specific_task),
 ]
