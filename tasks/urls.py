cce73a5b1df9691ef42935ff84d1d54173f7ea3c
Riaz Ahmed <riazahmed3919@gmail.com>
2025-06-16 01:16:28 +0600

    urls bug fixes

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import show_task, show_specific_task
 
 urlpatterns = [
-    path('show_task/', show_task)
+    path('show_task/', show_task),
     path('show_task/<int:id>/', show_specific_task),
 ]
